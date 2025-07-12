3f4e4486fbb85277268d56bf0413632ecfd3dc26
Syed Hashir Ali <pikaop125@gmail.com>
2025-07-13 01:00:07 +0500

    Integrated PUT method for products

---
+++ app/api/products/[productid]/route.js
@@ -0,0 +1,30 @@
+import connString from "@/lib/db";
+import { Product } from "@/lib/model/product";
+import mongoose from "mongoose";
+import { NextResponse } from "next/server";
+
+export async function PUT(request, content) {
+  const productId = await content.params.productid;
+
+  const filter = { _id: productId };
+  const payload = await request.json();
+
+  // Conect MongoDB:
+  await mongoose.connect(connString);
+  const result = await Product.findOneAndUpdate(filter, payload);
+
+  return NextResponse.json({ result, success: true });
+}
+
+export async function GET(request, content) {
+  
+  const productId = await content.params.productid;
+
+  const id = { _id: productId };
+ 
+  // Conect MongoDB:
+  await mongoose.connect(connString);
+  const result = await Product.findById(id);
+
+  return NextResponse.json({ result, success: true });
+}
